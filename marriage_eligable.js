53d724fbef1c4136755e1342a7da6a55cb450787
bopathirs <boopathirs111@gmail.com>
2022-06-08 20:58:27 +0530

    task6

---
+++ marriage_eligable.js
@@ -0,0 +1,17 @@
+let gender = 'female';
+let maleage = 20;
+let femaleage = 19;
+if (gender=='male'){
+if(maleage>=20){
+    console.log(`male age is ${maleage} can eligable for marriage`);
+}else{
+    console.log(`male age is ${maleage} cannot eligable for marriage`);
+}
+}
+else if(gender=='female'){
+if(femaleage>=18){
+    console.log(`female age is ${femaleage} can eligable for marriage`);
+}else{
+    console.log(`female age is ${femaleage} cannot eligable for marriage`);
+}
+}
